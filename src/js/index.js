d862e9e5a2b4fc4df4ae23664861b8fcd28816cc
vishnumd91 <vishnudivakaran1@gmail.com>
2023-02-12 16:59:11 +0530

    added previous and next functionalities with bugs

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,30 @@ import quizData from "../data/dataset.json" assert { type: "json" };
 
 const { music, coding, "modern-art": modern_art } = quizData;
 
-let currentQestionIndex = 0;
+let currentQuestionIndex = 0;
+let currentQuestionCount = 1;
 
 const questionContainer = document.getElementById("questionArea");
 const choiceElement = document.getElementById("choices");
 const totalQuestionElement = document.getElementById("totalQuestion");
+const currentQuestionElement = document.getElementById("currentQuestion");
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const submitButton = document.getElementById("submit");
+
+previousButton.style.display = "none";
+submitButton.style.display = "none";
+currentQuestionElement.innerText = currentQuestionCount;
+
+previousButton.addEventListener("click", previous);
+nextButton.addEventListener("click", next);
+submitButton.addEventListener("click", submit);
+
 // Data Selection
 
 // Display the Question
 function displayQuestion() {
-  const currentQuestion = music[currentQestionIndex];
+  const currentQuestion = music[currentQuestionIndex];
   questionContainer.innerText = currentQuestion.question;
   totalQuestionElement.innerText = music.length;
   const optionsHTML = currentQuestion.options
@@ -22,17 +36,47 @@ function displayQuestion() {
             </div>`;
     })
     .join("");
-
   choiceElement.innerHTML = optionsHTML;
 }
 
 displayQuestion();
 
-// Previous Button Operation
+// Previous Button
+function previous() {
+  // Show or hide the "Previous" button based on the current question index
+  if (currentQuestionIndex === 1) {
+    previousButton.style.display = "none";
+  } else {
+    previousButton.style.display = "inline-block";
+  }
+  currentQuestionCount--;
+  currentQuestionElement.innerText = currentQuestionCount;
+
+  currentQuestionIndex--;
+  displayQuestion();
+}
 
 // Next Button
+function next() {
+  if (currentQuestionIndex === music.length - 2) {
+    nextButton.style.display = "none";
+  } else {
+    nextButton.style.display = "inline-block";
+  }
+  currentQuestionCount++;
+  currentQuestionElement.innerText = currentQuestionCount;
 
+  currentQuestionIndex++;
+  displayQuestion();
+  previousButton.style.display = "inline-block";
+  console.log(currentQuestionIndex);
+}
 // Submit Button
+function submit() {
+  previousButton.style.display = "none";
+  nextButton.style.display = "none";
+  console.log(submitButton);
+}
 
 // Score Check
 
