8e1e4687ab6f595d2b160d5e3c6eb2e0277987f5
vishnumd91 <vishnudivakaran1@gmail.com>
2023-02-12 22:50:49 +0530

    checking the right answer for each questions implemented

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,14 @@ import data from "../data/dataset.json" assert { type: "json" };
 
 const { music, coding, "modern-art": modern_art } = data;
 
-let quizData;
+let quizData = [];
 
 quizData = [...music];
 
 let currentQuestionIndex = 0;
 let currentQuestionCount = 1;
+let wrongAnswers = 0;
+let currentQuestion;
 
 const questionContainer = document.getElementById("questionArea");
 const choiceElement = document.getElementById("choices");
@@ -16,6 +18,11 @@ const currentQuestionElement = document.getElementById("currentQuestion");
 const previousButton = document.getElementById("previous");
 const nextButton = document.getElementById("next");
 const submitButton = document.getElementById("submit");
+const musicButton = document.getElementById("musicType");
+const artButton = document.getElementById("artType");
+const codingButton = document.getElementById("codingType");
+
+console.log(musicButton);
 
 previousButton.style.display = "none";
 submitButton.style.display = "none";
@@ -24,26 +31,70 @@ currentQuestionElement.innerText = currentQuestionCount;
 previousButton.addEventListener("click", previous);
 nextButton.addEventListener("click", next);
 submitButton.addEventListener("click", submit);
+let scoreValue = 0;
 
-// Data Selection
+const score = localStorage.setItem("score", scoreValue);
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   musicButton.addEventListener("click", mapToQuizTypes("music"));
+//   artButton.addEventListener("click", mapToQuizTypes("art"));
+//   codingButton.addEventListener("click", mapToQuizTypes("coding"));
+// });
+
+// // Data Selection
+// function mapToQuizTypes(quizType) {
+//   console.log("hell");
+//   location.href = "quiz.html";
+//   switch (quizType) {
+//     case "music":
+//       quizData = [...music];
+//       break;
+//     case "art":
+//       quizData = [...modern_art];
+//     case "coding":
+//       quizData = [...coding];
+//     default:
+//       quizData = [];
+//       break;
+//   }
+// }
 
 // Display the Question
 function displayQuestion() {
-  const currentQuestion = quizData[currentQuestionIndex];
+  currentQuestion = quizData[currentQuestionIndex];
   questionContainer.innerText = currentQuestion.question;
   totalQuestionElement.innerText = quizData.length;
   const optionsHTML = currentQuestion.options
-    .map((choice, index) => {
+    .map((choice, index, answer) => {
+      console.log("answer", answer === currentQuestion.answer);
       return `<div class="input1">
-              <input id="option${index}" type="radio" name="options" />
+              <input class="radioInput" id="option${index}" type="radio" name="options" value=${choice
+        .split(" ")
+        .join("")} />
               <label for="option${index}">${choice}</label>
             </div>`;
     })
     .join("");
   choiceElement.innerHTML = optionsHTML;
+  checkAnswer();
 }
 
 displayQuestion();
+
+// Check for the selected option
+function checkAnswer() {
+  const radioButtons = document.querySelectorAll('input[type="radio"]');
+  radioButtons.forEach((button) => {
+    button.addEventListener("change", function () {
+      const selectedOption = document.querySelector(
+        'input[name="options"]:checked'
+      ).value;
+      if (selectedOption === currentQuestion.answer.replace(/ /g, "")) {
+        scoreValue++;
+      }
+    });
+  });
+}
 
 // Previous Button
 function previous() {
@@ -75,6 +126,7 @@ function next() {
 
   currentQuestionIndex++;
   displayQuestion();
+
   previousButton.style.display = "inline-block";
   console.log(currentQuestionIndex);
 }
