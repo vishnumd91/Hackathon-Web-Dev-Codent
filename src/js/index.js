fa8a1b9cf47118b9d189284d402edab5a366d2c6
vishnumd91 <vishnudivakaran1@gmail.com>
2023-02-12 14:12:12 +0530

    display question phase 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,39 @@
-function routing(){
-  window.location.href="./quiz.html"
-  
+import quizData from "../data/dataset.json" assert { type: "json" };
+
+const { music, coding, "modern-art": modern_art } = quizData;
+
+let currentQestionIndex = 0;
+
+const questionContainer = document.getElementById("questionArea");
+const choiceElement = document.getElementById("choices");
+const totalQuestionElement = document.getElementById("totalQuestion");
+// Data Selection
+
+// Display the Question
+function displayQuestion() {
+  const currentQuestion = music[currentQestionIndex];
+  questionContainer.innerText = currentQuestion.question;
+  totalQuestionElement.innerText = music.length;
+  const optionsHTML = currentQuestion.options
+    .map((choice, index) => {
+      return `<div class="input1">
+              <input id="option${index}" type="radio" name="options" />
+              <label for="option${index}">${choice}</label>
+            </div>`;
+    })
+    .join("");
+
+  choiceElement.innerHTML = optionsHTML;
 }
+
+displayQuestion();
+
+// Previous Button Operation
+
+// Next Button
+
+// Submit Button
+
+// Score Check
+
+// Loacal Storage for results
